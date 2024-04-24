c8df9a2556aad3ebbd0d328cf22f2b0e88126acf
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-24 19:48:12 +0200

    add prisma db connection/module

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+
+  res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+  const note = await prisma.note.create({
+    data: { title, content },
+  });
+  res.json(note);
+});
+
+app.listen(5000, () => {
+  console.log("server running on localhost:5000");
+});
